6bb58f04c21b3d5c1dd79cd46293c4e020f7056e
leonardoCavalcantii <leofilho206@gmail.com>
2024-12-09 23:30:23 -0300

    testando a criacao de testes usando o gerenceiador de projetos maven

---
+++ processadorDeContas/src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test01(){
+        Main main = new Main();
+        main.soma(10,20);
+    }
+}
