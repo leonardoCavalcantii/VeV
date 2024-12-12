56c78f3ac11e92c0c5318509cc9c64c711ba5343
leonardoCavalcantii <leofilho206@gmail.com>
2024-12-12 11:59:06 -0300

    adicao de mais testes em fatura para validar a sua criacao correta

--- processadorDeContas/src/test/java/FaturaTest.java
+++ processadorDeContas/src/test/java/FaturaTest.java
@@ -1,14 +1,21 @@
 import org.example.Fatura;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.assertEquals;
 
 public class FaturaTest {
 
     //toda fatura por defalt eh iniciada como PENDENTE.
+    //adicao de novos testes para checar funcionamento correto da criacao da fatura!
     @Test
     public void test01(){
         Fatura fatura = new Fatura("Joao Silva", 1500.0, "2020-02-17" );
+        assertEquals("Joao Silva", fatura.getCliente());
+        assertEquals(1500.0, fatura.getValor, 0.0001);
+        assertEquals(LocalDate.of(2020, 12, 4), fatura.getData());
         assertEquals("PENDENTE", fatura.getEstado());
+
     }
 }
