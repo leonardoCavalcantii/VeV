b2dec40237cf6312a441add0c40455eeeefff8ac
EmanuelSal <limaemanuel06@gmail.com>
2024-12-13 00:36:07 -0300

    testes de tipos de ingressos

---
+++ sistemaDeIngressos/src/test/java/LoteTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+public class LoteTest {
+
+    private Lote lote;
+
+    @Test
+    void calcularPrecoFinalComDescontoParaVip() {
+        lote = new Lote(1, 0.15);
+        double precoFinal = lote.calcularPrecoFinal("VIP", 20.0);
+        assertEquals(17.0, precoFinal, 0.01);
+    }
+}
