03917b70d468a6f07263dc92a05893124f5d9bef
EmanuelSal <limaemanuel06@gmail.com>
2024-12-13 00:59:23 -0300

    testando excessoes

--- sistemaDeIngressos/src/test/java/LoteTest.java
+++ sistemaDeIngressos/src/test/java/LoteTest.java
@@ -1,12 +1,16 @@
 import org.example.Lote;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoteTest {
 
     private static final String TIPO_VIP = "VIP";
+    private static final String TIPO_MEIA_ENTRADA = "MEIA_ENTRADA";
     private static final double PRECO_VIP = 20.0;
+    private static final double PRECO_MEIA_ENTRADA = 5.0;
     private static final double DESCONTO = 0.15;
 
     private Lote lote;
@@ -17,15 +21,32 @@ public class LoteTest {
     }
 
     @Test
-    void calcularPrecoFinalComDescontoParaVip() {
+    void CalcularPrecoFinalComDescontoParaVip() {
         double precoFinal = lote.calcularPrecoFinal(TIPO_VIP, PRECO_VIP);
-        assertEquals(17.0, precoFinal, 0.01);
+        assertEquals(17.0, precoFinal, 0.01); // 20 - 15%
     }
 
     @Test
     void naoDeveAplicarDescontoParaMeiaEntrada() {
-        Lote lote = new Lote(1, 0.15);
-        double precoFinal = lote.calcularPrecoFinal("MEIA_ENTRADA", 5.0);
-        assertEquals(5.0, precoFinal, 0.01);
+        double precoFinal = lote.calcularPrecoFinal(TIPO_MEIA_ENTRADA, PRECO_MEIA_ENTRADA);
+        assertEquals(5.0, precoFinal, 0.01); // Sem desconto
+    }
+
+    @Test
+    void excecaoParaPrecoNegativo() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> lote.calcularPrecoFinal("NORMAL", -10.0)
+        );
+        assertEquals("Tipo de ingresso ou preço inválido", exception.getMessage());
+    }
+
+    @Test
+    void excecaoParaTipoNulo() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> lote.calcularPrecoFinal(null, 10.0)
+        );
+        assertEquals("Tipo de ingresso ou preço inválido", exception.getMessage());
     }
 }
