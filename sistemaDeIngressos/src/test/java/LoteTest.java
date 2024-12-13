9b6fb09adfedb53f14773ab1d5329268dccbc235
EmanuelSal <limaemanuel06@gmail.com>
2024-12-13 00:49:33 -0300

    testes para lote continuando

--- sistemaDeIngressos/src/test/java/LoteTest.java
+++ sistemaDeIngressos/src/test/java/LoteTest.java
@@ -1,16 +1,31 @@
-
 import org.example.Lote;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoteTest {
 
+    private static final String TIPO_VIP = "VIP";
+    private static final double PRECO_VIP = 20.0;
+    private static final double DESCONTO = 0.15;
+
     private Lote lote;
+
+    @BeforeEach
+    void setup() {
+        lote = new Lote(1, DESCONTO);
+    }
 
     @Test
     void calcularPrecoFinalComDescontoParaVip() {
-        lote = new Lote(1, 0.15);
-        double precoFinal = lote.calcularPrecoFinal("VIP", 20.0);
+        double precoFinal = lote.calcularPrecoFinal(TIPO_VIP, PRECO_VIP);
         assertEquals(17.0, precoFinal, 0.01);
+    }
+
+    @Test
+    void naoDeveAplicarDescontoParaMeiaEntrada() {
+        Lote lote = new Lote(1, 0.15);
+        double precoFinal = lote.calcularPrecoFinal("MEIA_ENTRADA", 5.0);
+        assertEquals(5.0, precoFinal, 0.01);
     }
 }
